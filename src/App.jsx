73ee9246e642568def816c3a4e7b601728a721ed
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2023-10-17 10:08:25 +0530

    lifted up the state to App component. Show status message to the user, calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import './styles.scss';
 import Board from './components/Board';
+import { useState } from 'react';
+import { calculateWinner } from './CalculateWinner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const nextPlayer = isXNext ? 'X' : 'O';
+
+  const winner = calculateWinner(squares);
+
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+    setIsXNext(currentIsNext => !currentIsNext);
+  };
+
   return (
     <>
       <div className="app">
-        <Board />
+        <h1> {statusMessage} </h1>
+        <Board squares={squares} handleSquareClick={handleSquareClick} />
       </div>
     </>
   );
--- src/components/Board.1.jsx
+++ src/components/Board.1.jsx
@@ -1,6 +1,6 @@
 import Square from './Square';
 
-const Board = ({ squares, handleSquareClick }) => {
+export const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
@@ -30,5 +30,3 @@ const Board = ({ squares, handleSquareClick }) => {
     </div>
   );
 };
-
-export default Board;
